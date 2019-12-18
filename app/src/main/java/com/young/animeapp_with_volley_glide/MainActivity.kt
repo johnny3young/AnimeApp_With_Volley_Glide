4c79a54c7076af86f1f452119f19be1b433e8dd5
Black3 <youngerblack@gmail.com>
2019-12-17 20:48:57 -0500

    adding new project about glide and volley

---
+++ app/src/main/java/com/young/animeapp_with_volley_glide/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.young.animeapp_with_volley_glide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
